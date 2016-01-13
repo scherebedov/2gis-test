2fcb867bd7a679f4bdec2cdf777bd7be8af1194f
Sergey Cherebedov <cherebedov.sergey@gmail.com>
2016-01-14 03:38:38 +0600

    some code

--- index.js
+++ index.js
@@ -1,12 +1,86 @@
 var express = require("express");
+var WebSocketServer = new require('ws');
 var app = express();
 
 app.disable('x-powered-by');
 app.use(express.static('public'));
+app.listen(8090);
 
 
-app.get('/', function(req, res){
-
+var clients = new Array();
+var clientsNames = new Array();
+var history = {};
+var webSocketServer = new WebSocketServer.Server({
+  port: 9000
 });
 
-app.listen(8090);
+webSocketServer.on('connection', function(ws) {
+
+  var id = parseInt(Math.random().toString().slice(0,10) * 10000);
+  clients[id] = ws;
+
+  ws.on('message', function(msg) {
+    if (msg.length > 0) {
+      msg = JSON.parse(msg);
+      if (msg.type != undefined && msg.type.length > 0 && msg.data != undefined && msg.data.length > 0) {
+        switch (msg.type) {
+          case 'checkName':
+            if (clientsNames.indexOf(msg.data) !== -1) {
+              ws.send(JSON.stringify({
+                type: 'checkName',
+                data: false
+              }));
+              delete clients[id];
+            }
+            else {
+              clientsNames[id] = msg.data;
+              var userList = clientsNames.reduce(function(o, v, i) {
+                o[i] = v;
+                return o;
+              }, {});
+              ws.send(JSON.stringify({
+                type: 'checkName',
+                data: true,
+                users: userList,
+                history: {}
+              }));
+              for (var key in clients) {
+                clients[key].send(JSON.stringify({
+                  type: 'newUser',
+                  data: msg.data,
+                  users: userList
+                }));
+              }
+            }
+            break;
+          case 'newMsg':
+            for (var key in clients) {
+              clients[key].send(JSON.stringify({
+                type: 'newMsg',
+                name: clientsNames[id],
+                data: msg.data
+              }));
+            }
+            break;
+        }
+      }
+    }
+  });
+
+  ws.on('close', function() {
+    delete clients[id];
+    var leaveUserName = clientsNames[id];
+    delete clientsNames[id];
+    for (var key in clients) {
+      var userList = clientsNames.reduce(function(o, v, i) {
+        o[i] = v;
+        return o;
+      }, {});
+      clients[key].send(JSON.stringify({
+        type: 'leaveUser',
+        data: leaveUserName,
+        users: userList
+      }));
+    }
+  });
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,116 @@
-document.addEventListener("DOMContentLoaded", function(event) {
++function(){
+  window.Ui = function(){
+    var myName = '';
+    var setUsers = function(users) {
+      document.getElementById("user-list").innerHTML = Object.keys(users).map(function (key) {return users[key]}).reduce(function(html, current, i){
+        return html + '<li class="list-group-item">' + current + '</li>';
+      }, '');
+    };
+    var currentTime = function() {
+      var time = new Date();
+      return time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() ;
+    };
+    return {
+      error: function(text) {
+        document.getElementById("error-place").innerHTML = '<div class="alert alert-danger" role="alert">\
+          <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>\
+          <span class="sr-only">Error:</span>' + text + '</div>';
+      },
+      goMeet: function(newName, users) {
+        myName = newName;
+        document.getElementById("welcome-page").className = 'element-hidden';
+        document.getElementById("meeting-page").className = '';
+        setUsers(users)
+      },
+      setUsers: function(users) {
+        setUsers(users);
+      },
+      addUser: function(name, users) {
+        var message = document.createElement('p');
+        message.className = 'message event';
+        message.innerHTML = '<span class="time">' + currentTime() + '</span> \
+                             <span class="marker">' + name + '</span> присоеденился к чату';
+        document.getElementById("history").appendChild(message);
+        setUsers(users);
+      },
+      delUser: function(name, users) {
+        var message = document.createElement('p');
+        message.className = 'message event';
+        message.innerHTML = '<span class="time">' + currentTime() + '</span> \
+                             <span class="marker">' + name + '</span> покинул чат';
+        document.getElementById("history").appendChild(message);
+        setUsers(users);
+      },
+      addMsg: function(name, text) {
+        console.log(name + ' ' + text);
+        var message = document.createElement('p');
+        var msgIsMy = name == myName;
+        message.className = 'message ' + (msgIsMy ? 'pull-right' : 'pull-left');
+        message.innerHTML = '<span class="time ' + (msgIsMy ? 'pull-left' : 'pull-right') + '">' + currentTime() +'</span>\
+          <span class="marker">' + name + '</span>\
+          <span class="text">' + text + '</span>';
+        document.getElementById("history").appendChild(message);
+      },
+      getMyName: function() {
+        return myName;
+      }
+    }
 
+  };
+}();
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var ui = new Ui();
+  var socket = new WebSocket('ws://127.0.0.1:9000');
+
+  socket.onopen = function() {
+    document.getElementById("login").onclick = function() {
+      var name = document.getElementById("user-name").value;
+      if (name.length > 0) {
+        socket.send(JSON.stringify({
+          type: "checkName",
+          data: name
+        }));
+        socket.onmessage = function(msg) {
+          console.log(msg);
+          if (msg.data != undefined && msg.data.length > 0) {
+            var res = JSON.parse(msg.data);
+            switch (res.type) {
+              case 'checkName':
+                if (res.data) {
+                  ui.goMeet(name, res.users);
+                  document.getElementById("send-message").onclick = function() {
+                    var textMsg = document.getElementById("message-text").value;
+                    document.getElementById("message-text").value = '';
+                    if (textMsg != undefined && textMsg.length > 0) {
+                      socket.send(JSON.stringify({
+                        type: "newMsg",
+                        data: textMsg
+                      }));
+                    }
+                  };
+                }
+                else {
+                  ui.error("К сожалению, в настоящий момент этот псевдоним занят, пожалуйста, введите другой псевдоним или зайдите позже.");
+                }
+                break;
+              case 'newUser':
+                ui.addUser(res.data, res.users);
+                break;
+              case 'newMsg':
+                ui.addMsg(res.name, res.data);
+                break;
+              case 'leaveUser':
+                ui.delUser(res.data, res.users);
+                break;
+            }
+          }
+
+        };
+      }
+      else {
+        ui.error("Введите псевдоним, поле не может быть пустым.");
+      }
+    };
+  };
 });
